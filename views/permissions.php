e847ed9f59e8c60e21c84f7ebadc6cd883b51ef4
David William <david@uniplac.net>
2021-09-13 11:50:08 -0300

    Projeto NOVO

---
+++ views/permissions.php
@@ -0,0 +1,20 @@
+    <div class = "tabarea">
+
+        <div class = "tabitem activetab">Grupos de Permissões</div>
+
+        <div class = "tabitem">Permissões</div>
+
+    </div>
+    
+    <div class="tabcontent">
+
+        <div class="tabbody">
+            grupos de permissoes
+        </div>
+
+        <div class="tabbody">
+            permissoes
+        </div>
+
+    </div> 
+
---
+++ views/template.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Painel - <?php echo $viewData['company_name'];?></title>
+        <link href="<?php echo BASE_URL; ?>/assets/css/template.css" rel="stylesheet"/>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/jquery.js"></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/script.js"></script>
+    </head>
+    <body>
+
+        <div class="leftmenu">
+
+            <div class="company_name">
+
+                <?php echo $viewData['company_name'];?>
+
+            </div>
+
+            <div class="menuarea">
+
+                <ul>
+                    <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/permissions">Permissões</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+        <div class="container"> 
+
+            <div class="top">   
+
+                <div class="top_right">
+                    <a href="<?php echo BASE_URL.'/login/logout';?>">Sair</a>
+                </div>
+                
+                <div class="top_right">
+                    <?php echo $viewData['user_email'];?>     
+                </div>
+
+            </div>
+            
+            <div class="area">
+                <?php $this->loadViewInTemplate($viewName, $viewData);?>      
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
